a07a725dbbe3b41c41ea1c884004c36070dbb279
CodyCSUMB <codhopkins@csumb.edu>
2025-06-28 15:01:32 -0700

    finish part 3 shapeFactory (#3)
    Co-authored-by: Katz100 <cody.hopkins108@gmail.com>

---
+++ src/main/java/ShapeFactory.java
@@ -0,0 +1,36 @@
+/*
+ * @author Cody Hopkins
+ * Date: 06/28/2025
+ * Explanation: This class contains static methods for creating shapes.
+ */
+public class ShapeFactory {
+    //This will be used to reference values from a String[]
+    public static final int SHAPE_TYPE = 0;
+    public static final int SHAPE_WIDTH = 1;
+    public static final int SHAPE_HEIGHT = 2;
+    //Check to make sure there are the right number of tokens
+    public static final int TOKEN_COUNT = 3;
+
+    Shape getShape(String shapeString) {
+        String[] shapeTokens = shapeString.split(",");
+        if (shapeTokens.length != TOKEN_COUNT) {
+            System.out.printf("Required number of tokens %d actual number of tokens %d is the String correct?: %s %n",
+                    TOKEN_COUNT,
+                    shapeTokens.length,
+                    shapeString);
+            return null;
+        }
+        String shapeType = shapeTokens[SHAPE_TYPE];
+        int width = Integer.parseInt(shapeTokens[SHAPE_WIDTH]);
+        int height = Integer.parseInt(shapeTokens[SHAPE_HEIGHT]);
+
+        if (shapeType.equals("rectangle")) {
+            return new Rectangle(width, height);
+        } else {
+            System.out.printf("I don't know what %s is", shapeType);
+            return null;
+        }
+    }
+
+}
+
