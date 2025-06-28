73ab0c50138a8742a46e16ac5afafa7bd67b91b1
CodyCSUMB <codhopkins@csumb.edu>
2025-06-28 14:47:30 -0700

    finish part 2, shape interface (#2)
    Co-authored-by: Katz100 <cody.hopkins108@gmail.com>

---
+++ src/main/java/Shape.java
@@ -0,0 +1,12 @@
+/*
+ * @author Cody Hopkins
+ * Date: 06/28/2025
+ * Explanation: This is a generic shape class that has methods to operate on shapes
+ */
+public interface Shape {
+    /*
+     * Calculates the area of a shape using a specific rquation
+     * @return the area of a shape
+     */
+    int getArea();
+}
